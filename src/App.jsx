026f7f7fc3cec7e5b39c66a668d6e9a5d9a62207
ramkr88 <ramkblr88@gmail.com>
2025-10-16 13:04:12 +0530

    Committed Taskform with changes on 16th October

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+
+import Taskform from "./Components/Taskform";
+import TaskList from "./Components/TaskList";
+import Progresstracker from "./Components/Progresstracker";
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    localStorage.setItem
+    ("tasks", JSON.stringify(tasks))
+  });
+
+  const addTask = (task) => {
+    setTasks([...tasks,task]);
+  }
+
+  return(
+    <div>
+      <h1>Task-a-Lot</h1>
+      <p>Our friendly TaskManager</p>
+      <Taskform addTask = {addTask}/>
+      <TaskList />
+      <Progresstracker />
+      <button>Clear all tasks</button>
+    </div>
+  )
+}
+
---
+++ src/Components/TaskList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TaskList() {
+  return (
+    <div>
+      <h1>This is a task list component</h1>
+    </div>
+  )
+}
